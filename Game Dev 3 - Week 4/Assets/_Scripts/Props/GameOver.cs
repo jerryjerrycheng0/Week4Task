db39c35504466335cfb85136e7273d743e9e33e3
jerryjerrycheng0 <jerryjerrycheng0@gmail.com>
2024-10-17 16:48:52 +0100

    Altered a huge bunch of GameOver scripts

--- Game Dev 3 - Week 4/Assets/_Scripts/Props/GameOver.cs
+++ Game Dev 3 - Week 4/Assets/_Scripts/Props/GameOver.cs
@@ -1,40 +1,33 @@
-using GameDevWithMarco.Enemies;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace GameDevWithMarco.ObserverPattern
 {
     public class GameOver : MonoBehaviour
     {
-        public GameObject[] objectsToActivateOnDeath;
+        [SerializeField] GameObject[] objectsToActivateOnDeath;
 
-        [SerializeField]KnightView knightView;
-        // Start is called before the first frame update
-        void Start()
+        public void Start()
         {
-            knightView = FindAnyObjectByType<KnightView>();
             foreach (var items in objectsToActivateOnDeath)
             {
                 items.gameObject.SetActive(false);
+                // Disables the assigned game objects to ensure the UI only appears when the player is spotted
             }
 
         }
 
-        // Update is called once per frame
-        void Update()
+        // Activates the Game Over UI
+        public void StartGameOver()
         {
-            if (knightView.hasPlayerBeenSpotted == true)
+
+            foreach (var items in objectsToActivateOnDeath)
             {
-                foreach (var items in objectsToActivateOnDeath)
-                {
-                    items.gameObject.SetActive(true);
-                    Debug.Log("GO ON");
-                }
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
+                items.gameObject.SetActive(true);
             }
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
     }
 }
